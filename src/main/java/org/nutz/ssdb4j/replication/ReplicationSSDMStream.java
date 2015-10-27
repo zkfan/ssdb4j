f4b986a63a5d49b1cdbec69466d6af8cff521cab
zkfan <zkfan@aliyun.com>
2015-10-27 11:16:32 +0800

    读写分离，当slave挂掉，从master读数据

--- src/main/java/org/nutz/ssdb4j/replication/ReplicationSSDMStream.java
+++ src/main/java/org/nutz/ssdb4j/replication/ReplicationSSDMStream.java
@@ -1,6 +1,8 @@
 package org.nutz.ssdb4j.replication;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Logger;
 
 import org.nutz.ssdb4j.spi.Cmd;
 import org.nutz.ssdb4j.spi.Response;
@@ -9,30 +11,71 @@ import org.nutz.ssdb4j.spi.SSDBStreamCallback;
 
 public class ReplicationSSDMStream implements SSDBStream {
 
-	protected SSDBStream master;
-	
-	protected SSDBStream slave;
-	
-	public ReplicationSSDMStream(SSDBStream master, SSDBStream slave) {
-		this.master = master;
-		this.slave = slave;
-	}
+    private static Logger logger = Logger.getLogger("ReplicationSSDMStream");
 
-	public Response req(Cmd cmd, byte[]... vals) {
-		if (cmd.isSlave())
-			return slave.req(cmd, vals);
-		return master.req(cmd, vals);
-	}
+    private static int CHECK_PER_SEC = 60;
 
-	public void callback(SSDBStreamCallback callback) {
-		master.callback(callback);
-	}
-	
-	public void close() throws IOException {
-		try {
-			master.close();
-		} finally {
-			slave.close();
-		}
-	}
+    protected SSDBStream master;
+
+    protected SSDBStream slave;
+
+    public AtomicBoolean isOnline = new AtomicBoolean(true);
+    
+    public Thread checkThread;
+
+    public ReplicationSSDMStream(SSDBStream master, SSDBStream slave) {
+        this.master = master;
+        this.slave = slave;
+        checkThread= new Thread(new CheckRunnable());
+        checkThread.setDaemon(true);
+        checkThread.start();
+    }
+    
+    private class CheckRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                if (!isOnline.get()) {
+                    try {
+                        slave.req(Cmd.ping);
+                        isOnline.set(true);
+                        logger.info("slave is online.");
+                    } catch (Exception e) {
+                        isOnline.set(false);
+                        logger.info("slave is offline.");
+                    }
+                }
+                try {
+                    Thread.sleep(CHECK_PER_SEC * 1000);
+                } catch (InterruptedException e) {
+                    logger.info(e.getMessage());
+                }
+            }
+        }
+
+    }
+
+    public Response req(Cmd cmd, byte[]... vals) {
+        if (cmd.isSlave() && isOnline.get()) {
+            try {
+                return slave.req(cmd, vals);
+            } catch (Exception e) {
+                logger.info("slave error: " + e);
+                isOnline.set(false);
+            }
+        }
+        return master.req(cmd, vals);
+    }
+
+    public void callback(SSDBStreamCallback callback) {
+        master.callback(callback);
+    }
+
+    public void close() throws IOException {
+        try {
+            master.close();
+        } finally {
+            slave.close();
+        }
+    }
 }
